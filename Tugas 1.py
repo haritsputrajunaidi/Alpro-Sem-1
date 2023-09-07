2d4ae027f1e9105e311a602134a24c2f46c8202a
haritsputrajunaidi <144315506+haritsputrajunaidi@users.noreply.github.com>
2023-09-07 13:41:37 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,17 @@
+Angka = int(input("Masukkan Angka = "))
+if Angka % 2 == 0:
+    print(Angka, "Adalah Bilangan Genap")
+else:
+    print(Angka, "adalah Bilangan Ganjil")
+
+#Awal = int(input("masukkan nilai awal = "))
+#Akhir = int(input("masukkan nilai awal = "))
+#x = []
+#y = []
+#for i in range (Awal, Akhir + 1):
+#    if i%2 == 0:
+#        x.append(i)
+#    if i%2 == 1:
+#        y.append(i)
+#print(x, "adalah bilangan genap")
+#print(y, "adalah bilangan ganjil")
